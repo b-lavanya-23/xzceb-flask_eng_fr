0d6d711a0da8d6d4592ca5792c0c3b20c7be05df
b-lavanya-23 <beerlalavanya@gmail.com>
2023-05-20 07:51:56 -0400

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -1,6 +1,8 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
+from ibm_watson import LanguageTranslatorV3
 import json
+import machinetranslation
 
 app = Flask("Web Translator")
 
@@ -8,17 +10,23 @@ app = Flask("Web Translator")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    translation=language_translator.translate(text=english_text, model_id='en-fr').get_result()
+    french_text = translation['translations'][0]['translation']
+    return french_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+    translation = language_translator.translate(text=french_text, model_id='fr-en').get_result()
+    english_text = translation['translations'][0]['translation']
+    return english_text
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
